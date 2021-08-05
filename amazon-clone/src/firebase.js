0a8a844f5d5d3786f93e8c765641014439bb5018
mohamad <msiysinyuy@gmail.com>
2021-08-05 02:26:52 +0100

    the amazon react project set up, fire base is completely set up

---
+++ amazon-clone/src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC_Hc4YBIhCmeAXVeYufthLPbYLrDg40fw",
+  authDomain: "challenge-da442.firebaseapp.com",
+  projectId: "challenge-da442",
+  storageBucket: "challenge-da442.appspot.com",
+  messagingSenderId: "97089635768",
+  appId: "1:97089635768:web:f1c43b1b86127c8a8fbb4d",
+  measurementId: "G-YPJKYDXX8N"
+};
