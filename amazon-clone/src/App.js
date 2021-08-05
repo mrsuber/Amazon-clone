52241e24a12d03e46be6de0c8a14834e6aad9c93
mohamad <msiysinyuy@gmail.com>
2021-08-05 15:33:36 +0100

    react router implement using switch and checkout rout made

--- amazon-clone/src/App.js
+++ amazon-clone/src/App.js
@@ -1,14 +1,28 @@
 import Header from './components/Header'
 import Home from './components/Home'
 import './App.css';
+import {BrowserRouter as Router, Switch,Route} from 'react-router-dom'
+
 
 function App() {
   return (
     //BEM
-    <div className="App">
-    <Header />
-    <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <Switch>
+        <Route path='/checkout'>
+            <Header />
+            <h1>I am checkout page</h1>
+        </Route>
+          <Route path='/'>
+              <Header />
+              <Home />
+          </Route>
+
+        </Switch>
+
+      </div>
+    </Router>
   );
 }
 
